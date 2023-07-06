692bed4f0e07a15d4061889b00fd3bfd2309a787
grebenik72 <125321689+grebenik72@users.noreply.github.com>
2023-07-06 08:04:10 +0000

    Deploying to gh-pages from @ grebenik72/goit-js-hw-09@52d3d34cfd51cabcd2c9bdc35e9f203f78392ec5 🚀

--- 01-color-switcher.3a242876.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=0,e=document.querySelector("body"),n=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");n.addEventListener("click",(function(n){click.target.disable=!0,t=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),a.addEventListener("click",(function(e){click.target.disable=!0,clearInterval(t)}))}();
-//# sourceMappingURL=01-color-switcher.3a242876.js.map
