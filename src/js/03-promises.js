786fad8468db29c90ce896b19ff0e633628832d3
grebenik72 <grebenik72@gmail.com>
2023-07-08 14:54:34 +0300

    finish3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+
+const selectors = {
+  form: document.querySelector('form.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+}
+
+selectors.form.addEventListener('click', onPromiseCreate);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onPromiseCreate(e) {
+  e.preventDefault();
+
+  let valueDelay = Number(selectors.delay.value);
+  let step = Number(selectors.step.value);
+  let amount = Number(selectors.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    let promiseDelay = valueDelay + step * i;
+
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
